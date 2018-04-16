f0c3ed0d16b37a4fcba26502f90e5dd13686d2f8
Jonathan Abramovich <jjbinksaz@jjs-macbook-pro.3500.dhcp.asu.edu>
2018-04-16 09:14:17 -0700

    Assignment 6 submission 2

--- A6/server.js
+++ A6/server.js
@@ -9,54 +9,75 @@ var port = 1234;
 var Client = require('node-rest-client').Client;
 
 var MS = require("mongoskin");
-var db = MS.db("mongodb://localhost:27017/ame470")
+var db = MS.db("mongodb://localhost:27017/ame470");
 
 
 app.get("/", function (req, res) {
     res.redirect("index.html");
 });
 
-app.get("/getFeedData", function (req, res) {
-  var url = req.query.url;
-  var client = new Client();
-  client.get(url, function (data, response) {
-    res.end(JSON.stringify(data)); // send response body
+app.get("/getImgData", function (req, res) {
+  var data = req.query;
+  var id = data.id;
+  db.collection("img").findOne({id:id}, function(err, result){
+        res.end(JSON.stringify(result));
   });
 });
 
 
-app.get("/getAllFeeds", function (req, res) {
-  db.collection('feeds').find().toArray(function(err, items) {
+app.get("/getAllImgs", function (req, res) {
+  db.collection('img').find().sort({id:-1}).toArray(function(err, items) {
+    console.log(err, items);
+    if(!items) items = [];
     res.send(items);
   });
 });
 
-app.get("/addFeed", function (req, res) {
+app.get("/addImg", function (req, res) {
   var data = req.query;
-  db.collection("feeds").insert(data, function(err, result){
+  db.collection("img").insert(data, function(err, result){
     res.send("1");
   });
 });
 
 
-app.get("/renameFeed", function (req, res) {
+app.get("/renameImg", function (req, res) {
   var data = req.query;
   var newName = data.name;
   var id = data.id;
   console.log(newName, id);
-  db.collection("feeds").findOne({id:id}, function(err, result){
+  db.collection("img").findOne({id:id}, function(err, result){
       result.name = newName;
-      db.collection("feeds").save(result, function(err){
+      db.collection("img").save(result, function(err){
         res.end("1");
       });
   });
 });
 
 
-app.get("/deleteFeed", function (req, res) {
+app.get("/updateImg", function (req, res) {
+  var data = req.query;
+  var keys = Object.keys(data);
+  var id = data.id;
+ 
+  db.collection("img").findOne({id:id}, function(err, result){
+      console.log(id, err, result);
+      for(var i = 0; i < keys.length; i++){
+        var key = keys[i];
+        result[key] = data[key];
+      }
+
+      db.collection("img").save(result, function(err){
+        res.end("1");
+      });
+  });
+});
+
+
+app.get("/deleteImg", function (req, res) {
   var data = req.query;
   var id = data.id;
-  db.collection("feeds").remove({id:id}, function(err, result){
+  db.collection("img").remove({id:id}, function(err, result){
      res.end("1");
   });
 });
